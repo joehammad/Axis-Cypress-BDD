fdbddae135e365424a732c6498309a85ecf64f32
joehammad <joehammad4@gmail.com>
2024-10-29 18:28:21 +0300

    mocha reports

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,31 @@
 const { defineConfig } = require("cypress");
 const cucumber = require('cypress-cucumber-preprocessor').default
+
+async function setupNodeEvents(on, config) {
+  on('file:preprocessor', cucumber());
+  require('cypress-mochawesome-reporter/plugin')(on);
+  return config;
+  
+}
+
 module.exports = defineConfig({
+  reporter:'cypress-mochawesome-reporter',
+  reporterOptions:{
+    reportDir:'cypress/reports',
+    charts:true,
+    reportPageTitle:'cypress cucumber test report',
+    embeddedScreenshots:true,
+    inlineAssets:true,
+    saveAllAttempts: false,
+    debug: true,
+
+  },
+
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-      on('file:preprocessor', cucumber())
-      on('before:run', () => {
-        const tags = process.env.TAGS || 'not @ignore';
-        config.env.TAGS = tags;
-      });
-      return config;
-    },
+    setupNodeEvents,
     specPattern: "cypress/e2e/Features/*.feature",
+    experimentalModifyObstructiveThirdPartyCode:true, 
+    defaultCommandTimeout: 10000,
     
     env: {
       TAGS: "not @ignore"
